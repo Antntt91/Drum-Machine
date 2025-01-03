1e0a5027f827fdefe32affc76ae6bf23b09294be
Trần Nguyễn Thị Thuỳ An <111492670+Antntt91@users.noreply.github.com>
2025-01-03 10:23:22 +0700

    .

--- build-a-drum-machine/src/App.tsx
+++ build-a-drum-machine/src/App.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import "./App.css";
 import { AudioClip } from "./types";
 import Drum from "./Drum";
@@ -65,7 +66,7 @@ function App() {
   };
   return (
     <div className="container" id="drum-machine" onKeyDown={playAudio}>
-      <h1>FCC Drum Machine</h1>
+      <h1>Drum Machine</h1>
       <div className="whole-drum">
         {audioClips.map((clip) => (
           <Drum audioClip={clip} key={clip.keyTrigger} />
